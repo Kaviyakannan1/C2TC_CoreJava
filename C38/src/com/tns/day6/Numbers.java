d3c81332758dacc13c81fe293778e965c3844abe
avign <avign@172.16.150.193>
2024-04-16 13:08:12 +0530

    day 6 tasks

---
+++ C38/src/com/tns/day6/Numbers.java
@@ -0,0 +1,21 @@
+package com.tns.day6;
+import java.util.*;
+
+
+public class Numbers {
+	    public static void printPattern(int n) {
+	        for (int i = 1; i <= n; i++) {
+	            for (int j = 1; j <= i; j++) {
+	                System.out.print(j + " ");
+	            }
+	            System.out.println();
+	        }
+	    }
+
+	    public static void main(String args[]) {
+	        int n = 5;
+	        printPattern(n);
+	    }
+	}
+
+
